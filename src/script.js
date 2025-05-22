6c0d2761e2f7979b8ac42fff0b44edf761120046
Matheus Carvalho <matheuscarvalho210404@outlook.com>
2025-05-21 22:14:15 -0300

    ajustando a seleção das visualizações

--- src/script.js
+++ src/script.js
@@ -2,6 +2,10 @@ import { loadRegions, loadCards } from "./utils.js";
 import { getRoutesByRegionId } from "./data.js";
 
 const regionsSelect = document.getElementById("regions-select");
+const regionButton = document.getElementById("region-screen");
+const routeButton = document.getElementById("route-screen");
+const pokemonButton = document.getElementById("pokemon-screen");
+const buttons = [pokemonButton, routeButton, regionButton];
 
 const regionsArray = await loadRegions();
 loadCards();
@@ -15,4 +19,19 @@ regionsSelect.addEventListener("change", async (event) => {
   selectedRegion.id = regionsSelect.value;
   selectedRegion.name = regionsSelect.options[regionsSelect.selectedIndex].textContent;
   const routesArray = await getRoutesByRegionId(selectedRegion.id);
+});
+
+regionButton.addEventListener("click", (event) => {
+    buttons.forEach(btn => btn.classList.remove("active"));
+    regionButton.classList.add("active");
+});
+
+routeButton.addEventListener("click", (event) => {
+    buttons.forEach(btn => btn.classList.remove("active"));
+    routeButton.classList.add("active");
+});
+
+pokemonButton.addEventListener("click", (event) => {
+    buttons.forEach(btn => btn.classList.remove("active"));
+    pokemonButton.classList.add("active");
 });
