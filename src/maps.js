9a3e6cce41f719a134c9a4d27875ca6dbb4ba7c5
Sillas Rocha <sillasrocha29@gmail.com>
2025-05-31 22:45:58 -0300

    adicionando o arquivo de mapas com regiões e dimensões

---
+++ src/maps.js
@@ -0,0 +1,15 @@
+
+const regions = [
+    "Kanto", "Johto", "Hoenn", "Sinnoh", "Unova", "Kalos"
+]
+
+// Largura X Altura
+const dimensions = {
+    "Kanto": [200, 160],
+    "Johto": [166, 144],
+    "Hoenn": [306, 221],
+    "Sinnoh": [216, 168],
+    "Unova": [256, 168],
+    "Kalos": [320, 210]
+}
+
