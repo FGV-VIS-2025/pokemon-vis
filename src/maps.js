b7d2ff910b23218f2e0f8797cb21c05242c60c7e
Sillas Rocha <sillasrocha29@gmail.com>
2025-06-01 12:46:04 -0300

    adicionando polígonos de kanto

--- src/maps.js
+++ src/maps.js
@@ -1,4 +1,3 @@
-
 const regions = [
     "Kanto", "Johto", "Hoenn", "Sinnoh", "Unova", "Kalos"
 ]
@@ -12,4 +11,72 @@ const dimensions = {
     "Unova": [256, 168],
     "Kalos": [320, 210]
 }
+
+const regionLocations = {
+    "Kanto": `
+<area shape="rect" coords="47, 112, 56, 103" title="Pallet Town">
+<area shape="rect" coords="47, 92, 56, 76" title="Viridian City">
+<area shape="rect" coords="47, 64, 56, 71" title="Viridian Forest">
+<area shape="rect" coords="47, 62, 56, 46" title="Pewter City">
+<area shape="rect" coords="47, 47, 62, 56" title="Pewter City">
+<area shape="rect" coords="88, 39, 96, 48" title="Mt. Moon">
+<area shape="rect" coords="127, 39, 137, 48" title="Cerulean City">
+<area shape="rect" coords="160, 40, 168, 47" title="Rock Tunnel">
+<area shape="rect" coords="160, 50, 168, 57" title="Power Plant">
+<area shape="rect" coords="160, 64, 168, 71" title="Lavender Town">
+<area shape="rect" coords="168, 74, 176, 82" title="Pokémon Tower">
+<area shape="rect" coords="124, 62, 138, 76" title="Saffron City">
+<area shape="rect" coords="104, 64, 111, 72" title="Celadon City">
+<area shape="rect" coords="128, 88, 136, 96" title="Vermilion City">
+<area shape="rect" coords="136, 88, 144, 96" title="Diglett's Cave">
+<area shape="rect" coords="112, 112, 120, 120" title="Fuchsia City">
+<area shape="rect" coords="75, 128, 88, 135" title="Seafoam Islands">
+<area shape="rect" coords="47, 128, 55, 136" title="Cinnabar Island">
+<area shape="rect" coords="23, 55, 31, 48" title="Victory Road">
+<area shape="rect" coords="23, 39, 31, 47" title="Indigo Plateau">
+<area shape="rect" coords="2, 109, 10, 116" title="Tohjo Falls">
+<area shape="rect" coords="0, 80, 8, 88" title="Mt. Silver (Johto)">
+<area shape="rect" coords="47, 103, 56, 92" title="Route 1">
+<area shape="rect" coords="47, 76, 56, 71" title="Route 2">
+<area shape="rect" coords="63, 47, 88, 56" title="Route 3">
+<area shape="rect" coords="79, 39, 88, 48" title="Route 4">
+<area shape="rect" coords="95, 39, 128, 48" title="Route 4">
+<area shape="rect" coords="127, 48, 137, 61" title="Route 5">
+<area shape="rect" coords="127, 77, 137, 88" title="Route 6">
+<area shape="rect" coords="139, 63, 159, 72" title="Route 7">
+<area shape="rect" coords="112, 63, 123, 72" title="Route 8">
+<area shape="rect" coords="136, 39, 159, 48" title="Route 9">
+<area shape="rect" coords="159, 39, 168, 64" title="Route 10">
+<area shape="rect" coords="144, 87, 159, 96" title="Route 11">
+<area shape="rect" coords="159, 72, 168, 104" title="Route 12">
+<area shape="rect" coords="143, 103, 168, 112" title="Route 13">
+<area shape="rect" coords="135, 103, 145, 120" title="Route 14">
+<area shape="rect" coords="120, 111, 135, 120" title="Route 15">
+<area shape="rect" coords="71, 63, 103, 72" title="Route 16">
+<area shape="rect" coords="71, 72, 80, 112" title="Route 17">
+<area shape="rect" coords="71, 111, 112, 120" title="Route 18">
+<area shape="rect" coords="111, 120, 121, 136" title="Route 19">
+<area shape="rect" coords="55, 127, 111, 137" title="Route 20">
+<area shape="rect" coords="47, 127, 56, 112" title="Route 21">
+<area shape="rect" coords="48, 79, 31, 88" title="Route 22">
+<area shape="rect" coords="22, 55, 32, 79" title="Route 23">
+<area shape="rect" coords="127, 40, 136, 23" title="Route 24">
+<area shape="rect" coords="136, 23, 152, 33" title="Route 25">
+<area shape="rect" coords="0, 103, 32, 118" title="Route 27">
+<area shape="rect" coords="22, 88, 32, 108" title="Route 26">
+<area shape="rect" coords="8, 79, 23, 88" title="Route 28">
+`,
+    "Johto": `
+`,
+    "Hoenn": `
+`,
+    "Sinnoh": `
+`,
+    "Unova": `
+`,
+    "Kalos": `
+`
+}
+
+
 
