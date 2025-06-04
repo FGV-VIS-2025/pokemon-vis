321b0f0a42684c6cac2d9c71d6740e0384050cbb
Matheus Carvalho <matheuscarvalho210404@outlook.com>
2025-06-04 12:39:57 -0300

    página dos autores e das instruções feita pela metade

--- pokedex-project/buttonsAnimation.js
+++ pokedex-project/buttonsAnimation.js
@@ -9,8 +9,18 @@ const rightPokemonButton = document.getElementsByClassName("home-pokemon")[0];
 const homePokemonButton = document.getElementsByClassName("right-pokemon")[0];
 const authorsButton = document.getElementsByClassName("authors-button")[0];
 const scrollButton = document.getElementsByClassName("scroll-button")[0];
+const linkedinButtons = document.getElementsByClassName("linkedin-button");
+const githubButtons = document.getElementsByClassName("github-button");
 
-const contentButtons = [regionButton, locationButton, pokemonButton, folderButton, leftPokemonButton, homePokemonButton, rightPokemonButton, scrollButton, authorsButton];
+const contentButtons = [regionButton, 
+                        locationButton, 
+                        pokemonButton, 
+                        folderButton, 
+                        leftPokemonButton, 
+                        homePokemonButton, 
+                        rightPokemonButton, 
+                        scrollButton, 
+                        authorsButton];
 
 function animatedButtons(arrayOfButtons){
     for (let eachButton of arrayOfButtons) {
@@ -38,12 +48,65 @@ function animatedButtons(arrayOfButtons){
 animatedButtons(leftButtons);
 animatedButtons(rightButtons);
 animatedButtons(contentButtons);
+animatedButtons(linkedinButtons);
+animatedButtons(githubButtons);
 
 // SCROLL AUTOMÁTICO
 scrollButton.addEventListener("click", function () {
-  document.getElementsByClassName("content-container")[0].scrollIntoView({ behavior: "smooth" });
+  setTimeout(function () {document.getElementsByClassName("content-container")[0].scrollIntoView({ behavior: "smooth" })}, 150);
 });
 
 authorsButton.addEventListener("click", function () {
-  document.getElementsByClassName("authors-container")[0].scrollIntoView({ behavior: "smooth" });
+  setTimeout(function () {document.getElementsByClassName("authors-container")[0].scrollIntoView({ behavior: "smooth" })}, 150);
+});
+
+// BOTÕES AUTORES
+const linkedinAlessandra = document.getElementById("linkedin-alessandra");
+const githubAlessandra = document.getElementById("github-alessandra");
+const linkedinMatheus = document.getElementById("linkedin-matheus");
+const githubMatheus = document.getElementById("github-matheus");
+const linkedinSillas = document.getElementById("linkedin-sillas");
+const githubSillas = document.getElementById("github-sillas");
+
+linkedinAlessandra.addEventListener("click", function () {
+    setTimeout(function () {
+        window.open("https://www.linkedin.com/in/alessandra-bello-soares/", "_blank");
+    }, 150);
+});
+
+githubAlessandra.addEventListener("click", function () {
+    setTimeout(function () {
+        window.open("https://github.com/AlessandraBello", "_blank");
+    }, 150);
+});
+
+linkedinMatheus.addEventListener("click", function () {
+    setTimeout(function () {
+        window.open("https://www.linkedin.com/in/matheus-carvalho-11095430b/", "_blank");
+    }, 150);
+});
+
+githubMatheus.addEventListener("click", function () {
+    setTimeout(function () {
+        window.open("https://github.com/MatCarvalho21", "_blank");
+    }, 150);
+});
+
+linkedinSillas.addEventListener("click", function () {
+    setTimeout(function () {
+        window.open("https://www.linkedin.com/in/scrocha/", "_blank");
+    }, 150);
+});
+
+githubSillas.addEventListener("click", function () {
+    setTimeout(function () {
+        window.open("https://github.com/scrocha", "_blank");
+    }, 150);
+});
+
+// FOLDER
+folderButton.addEventListener("click", function () {
+    setTimeout(function () {
+        window.open("https://github.com/FGV-VIS-2025/pokemon-vis", "_blank");
+    }, 150);
 });
